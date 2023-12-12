80e4519594d68e840063b32c45592b8d8453cc45
Nalynn Vk <134136410+NalynnVk@users.noreply.github.com>
2023-12-12 13:42:22 +0800

    update

--- app/Http/Resources/v1/MedicationResource.php
+++ app/Http/Resources/v1/MedicationResource.php
@@ -15,6 +15,7 @@ class MedicationResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            // 'id' => $this->id,
             'id' => $this->id,
             'name' => $this->name,
         ];
