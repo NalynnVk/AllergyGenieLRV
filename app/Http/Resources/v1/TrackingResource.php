38958435abca8e050ebe864d393f276c22922ba1
Nalynn Vk <134136410+NalynnVk@users.noreply.github.com>
2023-12-27 23:49:04 +0800

    Update TrackingResource.php

--- app/Http/Resources/v1/TrackingResource.php
+++ app/Http/Resources/v1/TrackingResource.php
@@ -17,6 +17,7 @@ class TrackingResource extends JsonResource
         return
             [
                 //symptom(name), allergen(name), symptom(severity_label), symptom(notes)
+                'id' => $this->id,
                 'symptom' => new SymptomResource($this->symptom),
                 'allergen' => new AllergenResource($this->allergen),
                 'severity_id' => $this->severity,
